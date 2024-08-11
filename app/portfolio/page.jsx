16cbe4399af9a3f1f2b2b7d3c17b845195dc5e71
Sergii Kovalov <sergiikovalov@MacBook-Pro-von-Sergii.local>
2024-08-11 17:14:01 +0200

    Cleaning after google api

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import MediaGallery from '../../components/MediaGallery';
+
 
 const PortfolioPage = () => {
-    const fotosFolderId = '1dpMWk6rEvmnkg3r7-1bbXutltkq-gQ6K'; // Replace with your actual folder ID
-    const videosFolderId = '1ja4rSeezsNh2naSUoNG7UC3sM5fgh0rA'; // Replace with your actual folder ID
-
-    return (
-        <div>
-            <h1>Fotos</h1>
-            <MediaGallery folderId={fotosFolderId} />
-
-            <h1>Videos</h1>
-            <MediaGallery folderId={videosFolderId} />
-        </div>
-    );
+ return <div></div>
 };
 
 export default PortfolioPage;
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'drive.google.com',
-                pathname: '/uc**',
-            },
-        ],
-    },
+
 };
 
 export default nextConfig;
